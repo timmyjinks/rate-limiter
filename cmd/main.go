2a41ecde3acd4a0b9f6131df18ec60606d8bcf68
timmyjinks <tysonjjenkins.github@gmail.com>
2026-03-30 22:30:21 -0600

    add basic rate limiting

--- cmd/main.go
+++ cmd/main.go
@@ -14,16 +14,25 @@ func Request(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
+	limiter := NewRateLimiter(5, 5)
+	http.HandleFunc("/", RateLimitMiddleware(limiter, Request))
+
 	fmt.Println("Listening on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func RateLimitMiddleware(next http.HandlerFunc) http.HandlerFunc {
+func RateLimitMiddleware(limiter *RateLimiter, next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		_, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
 			http.Error(w, "Invalid IP", http.StatusInternalServerError)
 			return
+		}
+
+		if limiter.Allow() {
+			next(w, r)
+		} else {
+			http.Error(w, "Too Many Requests", http.StatusTooManyRequests)
 		}
 	}
 }
--- cmd/rate.go
+++ cmd/rate.go
@@ -1,3 +1,40 @@
 package main
 
-import ()
+import (
+	"time"
+)
+
+type RateLimiter struct {
+	tokens         int
+	capacity       int
+	refillInterval int
+	lastRefillTime time.Time
+}
+
+func NewRateLimiter(capacity int, refillInterval int) *RateLimiter {
+	rl := &RateLimiter{
+		capacity:       capacity,
+		refillInterval: refillInterval,
+	}
+	return rl
+}
+
+func (r *RateLimiter) Allow() bool {
+	r.refillTokens()
+	if r.tokens > 0 {
+		r.tokens--
+		return true
+	}
+	return false
+}
+
+func (r *RateLimiter) refillTokens() {
+	now := time.Now()
+	elapsedTime := int(now.Sub(r.lastRefillTime) / time.Second)
+	refillTokens := elapsedTime / r.refillInterval
+
+	if refillTokens > 0 {
+		r.tokens = min(r.capacity, r.tokens+refillTokens)
+		r.lastRefillTime = now
+	}
+}
